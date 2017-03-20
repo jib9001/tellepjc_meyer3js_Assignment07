3df9bf0ffb6d31d55d7d7de214c6de6132660df8
Justin Meyer <meyer3js@mail.uc.edu>
2017-03-20 18:41:24 -0400

    Added initial listbox populated control

--- tellepjc_meyer3js_Assignment07/Assignment07.aspx.cs
+++ tellepjc_meyer3js_Assignment07/Assignment07.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dsStoreTableAdapters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,20 @@ public partial class tellepjc_meyer3js_Assignment07_Assignment07 : System.Web.UI
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
+        // Only run on initial page load
+        if (!IsPostBack)
+        {
+            // Get Computer case data and initialize first selected index in ddl.
+            tStoreTableAdapter storeTableAdapter = new tStoreTableAdapter();
+            dsStore.tStoreDataTable storeDataTable = storeTableAdapter.GetData();
+            lbStore.DataTextField = "StoreWithAddress";
+            lbStore.DataValueField = "StoreID";
+            lbStore.DataSource = storeDataTable;
+            lbStore.DataBind();
+           // lbStore.SelectedIndex = 0;
+            //GetParts(ddlComputerCase.SelectedValue);
+        }
 
     }
 }
