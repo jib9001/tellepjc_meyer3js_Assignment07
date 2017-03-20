278dd069f8dfdac1bba3777ea05db9eb669e39ff
Justin Meyer <meyer3js@mail.uc.edu>
2017-03-20 19:54:00 -0400

    Added logic for dynamic query generation - Stores

--- tellepjc_meyer3js_Assignment07/Assignment07.aspx.cs
+++ tellepjc_meyer3js_Assignment07/Assignment07.aspx.cs
@@ -8,6 +8,12 @@ using System.Web.UI.WebControls;
 
 public partial class tellepjc_meyer3js_Assignment07_Assignment07 : System.Web.UI.Page
 {
+    string minQty = "";
+    string maxQty = "";
+    string startDate = "";
+    string endDate = "";
+    string storeQueryList = "";
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
@@ -18,12 +24,49 @@ public partial class tellepjc_meyer3js_Assignment07_Assignment07 : System.Web.UI
             tStoreTableAdapter storeTableAdapter = new tStoreTableAdapter();
             dsStore.tStoreDataTable storeDataTable = storeTableAdapter.GetData();
             cbStore.DataTextField = "StoreWithAddress";
-            cbStore.DataValueField = "StoreID";
+            cbStore.DataValueField = "Store";
             cbStore.DataSource = storeDataTable;
             cbStore.DataBind();
-           // lbStore.SelectedIndex = 0;
+            // lbStore.SelectedIndex = 0;
             //GetParts(ddlComputerCase.SelectedValue);
+
+
         }
 
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        GenerateStores();
+        GenerateDateRange();
+        GenerateQtyRange();
+    }
+
+    private void GenerateQtyRange()
+    {
+        minQty = tbMinQty.Text;
+        maxQty = tbMaxQty.Text;
+    }
+
+    private void GenerateDateRange()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void GenerateStores()
+    {
+        bool hasRun = false;
+        foreach (ListItem item in cbStore.Items)
+        {
+            if (item.Selected && !hasRun)
+            {
+                storeQueryList += "dbo.tStore.Store = " + item.Value;
+                hasRun = true;
+            }
+            else if (item.Selected)
+            {
+                storeQueryList += "or dbo.tStore.Store = " + item.Value;
+            }
+        }
     }
 }
