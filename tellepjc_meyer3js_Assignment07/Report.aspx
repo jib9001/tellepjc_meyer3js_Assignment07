833e68d486766768a818374629cf1777448fba6b
Justin Meyer <kite.akcornstudios@gmail.com>
2017-03-22 01:55:05 -0400

    Added Result Dataset Added Report Webform

---
+++ tellepjc_meyer3js_Assignment07/Report.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/tellepjc_meyer3js_Assignment07/Assignment07.master" AutoEventWireup="true" CodeFile="Report.aspx.cs" Inherits="tellepjc_meyer3js_Assignment07_Report" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+
+</asp:Content>
+
---
+++ tellepjc_meyer3js_Assignment07/Report.aspx.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using dsResultTableAdapters;
+using System.Data.SqlClient;
+
+public partial class tellepjc_meyer3js_Assignment07_Report : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        dsResult result = new dsResult();
+        tResultTableAdapter resultTableAdapter = new tResultTableAdapter();
+        dsResult.tResultDataTable resultDataTable = new dsResult.tResultDataTable();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        adapter.SelectCommand = new SqlCommand(Convert.ToString(Session["Query"]));
+        adapter.Fill(result);
+        
+        
+        
+        /* result.GetData.
+         dsResult.tResultDataTable resultDataTable = storeTableAdapter.GetData();
+
+         cbStore.DataTextField = "StoreWithAddress";
+         cbStore.DataValueField = "Store";
+         cbStore.DataSource = storeDataTable;
+         cbStore.DataBind();*/
+
+    }
+}
