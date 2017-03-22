3fc9d9162a4d197feb367504e092d7520a584df9
Justin Meyer <kite.akcornstudios@gmail.com>
2017-03-22 02:20:46 -0400

    Created initial report file

--- tellepjc_meyer3js_Assignment07/Report.aspx.cs
+++ tellepjc_meyer3js_Assignment07/Report.aspx.cs
@@ -12,15 +12,9 @@ public partial class tellepjc_meyer3js_Assignment07_Report : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        dsResult result = new dsResult();
-        tResultTableAdapter resultTableAdapter = new tResultTableAdapter();
-        dsResult.tResultDataTable resultDataTable = new dsResult.tResultDataTable();
-        SqlDataAdapter adapter = new SqlDataAdapter();
-        adapter.SelectCommand = new SqlCommand(Convert.ToString(Session["Query"]));
-        adapter.Fill(result);
-        
-        
-        
+        PopulateDataSet();
+        GenerateReport();
+           
         /* result.GetData.
          dsResult.tResultDataTable resultDataTable = storeTableAdapter.GetData();
 
@@ -29,5 +23,20 @@ public partial class tellepjc_meyer3js_Assignment07_Report : System.Web.UI.Page
          cbStore.DataSource = storeDataTable;
          cbStore.DataBind();*/
 
+    }
+
+    private void PopulateDataSet()
+    {
+        dsResult result = new dsResult();
+        tResultTableAdapter resultTableAdapter = new tResultTableAdapter();
+        dsResult.tResultDataTable resultDataTable = new dsResult.tResultDataTable();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        adapter.SelectCommand = new SqlCommand(Convert.ToString(Session["Query"]));
+        adapter.Fill(result);
+    }
+
+    private void GenerateReport()
+    {
+        
     }
 }
