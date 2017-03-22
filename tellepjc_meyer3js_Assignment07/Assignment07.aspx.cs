b3748668c162f2c92eb0d7640f477db8760a7c1b
Justin Meyer <kite.akcornstudios@gmail.com>
2017-03-21 22:49:35 -0400

    Added comments Finished the GenerateDateRange method Created the GenerateQueryString method

--- tellepjc_meyer3js_Assignment07/Assignment07.aspx.cs
+++ tellepjc_meyer3js_Assignment07/Assignment07.aspx.cs
@@ -1,11 +1,23 @@
-﻿/*
-
-
-
-
-
-
-
+﻿/***********************************************************************************************************
+ * Assignment 07
+ * Connor Tellep and Justin Meyer
+ * IT3047C-001 Spring Semester
+ * tellepjc@mail.uc.edu | meyer3js@mail.uc.edu
+ * This assignment demonstrates our ability to create a query based on user input while also generating a 
+ * report from that input.
+ * This class contains the events necessary to create a dynamic query based on user input.
+ * Due Date: Wednesday, March 22nd, 2017 by 5:19pm
+ * Citations: 
+ * 
+ * 
+ * 
+ * REFERENCES:
+ * 
+ * 
+ * 
+ * ALWAYS remember the cat
+ * **********************************************************************************************************/
+/*
 -- Example query structure
 
 SELECT        dbo.tStore.Store, SUM(dbo.tTransactionDetail.QtyOfProduct) AS Expr1
@@ -31,11 +43,11 @@ using System.Web.UI.WebControls;
 
 public partial class tellepjc_meyer3js_Assignment07_Assignment07 : System.Web.UI.Page
 {
-    string minQty = "";
-    string maxQty = "";
-    string startDate = "";
-    string endDate = "";
-    string storeQueryList = "";
+    string minQty = ""; // The minimum quantity of products sold in a transaction
+    string maxQty = ""; // The maximum quantity of products sold in a transaction
+    string startDate = ""; // The starting date of a transaction that the query will look in the db
+    string endDate = ""; // The ending date of a transaction that the query will look in the db
+    string storeQueryList = ""; // The string that will contain the stores that the user selected
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -43,39 +55,50 @@ public partial class tellepjc_meyer3js_Assignment07_Assignment07 : System.Web.UI
         // Only run on initial page load
         if (!IsPostBack)
         {
-            // Get Computer case data and initialize first selected index in ddl.
+            // Populate the checked box with data from the store table
             tStoreTableAdapter storeTableAdapter = new tStoreTableAdapter();
             dsStore.tStoreDataTable storeDataTable = storeTableAdapter.GetData();
             cbStore.DataTextField = "StoreWithAddress";
             cbStore.DataValueField = "Store";
             cbStore.DataSource = storeDataTable;
             cbStore.DataBind();
-            // lbStore.SelectedIndex = 0;
-            //GetParts(ddlComputerCase.SelectedValue);
-
-
         }
-
     }
 
+    /// <summary>
+    /// Calls the methods to generate the query string before calling the method that combines them all together.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     protected void btnSubmit_Click(object sender, EventArgs e)
     {
         GenerateStores();
         GenerateDateRange();
         GenerateQtyRange();
+        GenerateQueryString();
     }
 
+    /// <summary>
+    /// Gathers the minimum and maximum product purchase quantities that will be ran against the database.
+    /// </summary>
     private void GenerateQtyRange()
     {
         minQty = tbMinQty.Text;
         maxQty = tbMaxQty.Text;
     }
 
+    /// <summary>
+    /// Gathers the dates selected by the user and formats them correctly.
+    /// </summary>
     private void GenerateDateRange()
     {
-        throw new NotImplementedException();
+        startDate = String.Format("{0}-{1}-{2}", cStartDate.SelectedDate.Year, cStartDate.SelectedDate.Month, cStartDate.SelectedDate.Day);
+        endDate = String.Format("{0}-{1}-{2}", cEndDate.SelectedDate.Year, cEndDate.SelectedDate.Month, cEndDate.SelectedDate.Day);
     }
 
+    /// <summary>
+    /// Traverses through the checked box list and appends "or" clauses for each store selected.
+    /// </summary>
     private void GenerateStores()
     {
         bool hasRun = false;
@@ -91,5 +114,12 @@ public partial class tellepjc_meyer3js_Assignment07_Assignment07 : System.Web.UI
                 storeQueryList += "or dbo.tStore.Store = " + item.Value;
             }
         }
+    }
+    /// <summary>
+    /// Combines the results of the other methods into a query string that will be ran against the database to generate a report on the next page.
+    /// </summary>
+    private void GenerateQueryString() 
+    {
+        
     }
 }
--- tellepjc_meyer3js_Assignment07/Assignment07.master.cs
+++ tellepjc_meyer3js_Assignment07/Assignment07.master.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿/***********************************************************************************************************
+ * Assignment 07
+ * Connor Tellep and Justin Meyer
+ * IT3047C-001 Spring Semester
+ * tellepjc@mail.uc.edu | meyer3js@mail.uc.edu
+ * This assignment demonstrates our ability to create a query based on user input while also generating a 
+ * report from that input.
+ * This class contains nothing currently.
+ * Due Date: Wednesday, March 22nd, 2017 by 5:19pm
+ * Citations: 
+ * 
+ * 
+ * 
+ * REFERENCES:
+ * 
+ * 
+ * 
+ * ALWAYS remember the cat
+ * **********************************************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
